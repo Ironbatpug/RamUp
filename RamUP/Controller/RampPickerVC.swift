a6c7a2a21e96461dbaf74b479e2252c6d954f33c
Ironbatpug <molnarcsaba92@gmail.com>
2019-09-15 11:29:05 +0200

    initial commit

---
+++ RamUP/Controller/RampPickerVC.swift
@@ -0,0 +1,13 @@
+//
+//  RampPickerVC.swift
+//  RamUP
+//
+//  Created by Molnár Csaba on 2019. 09. 15..
+//  Copyright © 2019. Molnár Csaba. All rights reserved.
+//
+
+import UIKit
+
+class RampPickerVC: UICollectionViewCell {
+    
+}
